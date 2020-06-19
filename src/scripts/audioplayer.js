ec95a41d6878991aebd60597d695f8eccf140d6d
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-06-19 14:08:02 +0200

    Add basic working Audioplayer
    Co-authored-by: Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>

--- src/scripts/audioplayer.js
+++ src/scripts/audioplayer.js
@@ -6,25 +6,42 @@ class Audioplayer {
     this.items = data.querySelectorAll('.playlist__item');
     this.songs = [];
 
-    this.items.forEach((item, index) => {
-      this.songs[index] = new Howl({
-        src: [item.getAttribute('[data-js-source]')],
-      });
-
-      item.addEventListener('click', this.play(index));
-    });
+    this.init();
   }
 
   init() {
+    this.items.forEach((item, index) => {
+      this.songs[index] = new Howl({
+        src: [item.getAttribute('data-js-source')],
+      });
 
+      item.addEventListener('click', () => {
+        this.switchState(index);
+      });
+    });
+  }
+
+  switchState(songID) {
+    if (this.items[songID].classList.contains('playlist__item--playing')) {
+      this.stop(songID);
+    } else {
+      this.play(songID);
+    }
   }
 
   play(songID) {
+    this.songs.forEach((song, id) => {
+      this.stop(id);
+    });
+
     this.songs[songID].play();
+    this.items[songID].classList.add('playlist__item--playing');
   }
 
   stop(songID) {
     this.songs[songID].stop();
+
+    this.items[songID].classList.remove('playlist__item--playing');
   }
 }
 
